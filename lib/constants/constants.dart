fc94cc7090d97bc1c5f33cca61061fe4cdf32a6c
azix khan <138978130+azix-khan@users.noreply.github.com>
2024-07-11 20:47:36 +0500

    Update constants.dart

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,5 +1,5 @@
 class AppInfo {
-  static const appId = 550485849;
+  static const appId = your appid here (from zegocloud console);
   static const appSign =
-      '6f5b9b58071e1b965770775c79a55699b8bbccbf3c62037a9ff6cfa6f106ec56';
+      'your appsign here';
 }
